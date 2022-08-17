585e1f1eec154d54522fa725742cf8a19e22d7ad
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-08-17 02:02:01 -0300

    Create FloatSorter.java

---
+++ ADS/DDD/Aulas/Generics/src/br/com/benefrancis/generics/FloatSorter.java
@@ -0,0 +1,19 @@
+package br.com.benefrancis.generics;
+
+import java.util.Random;
+
+public class FloatSorter {
+
+	float[] valores;
+
+	public FloatSorter(float[] valores) {
+		super();
+		this.valores = valores;
+	}
+
+	public double sortear() {
+		Random r = new Random();
+		return valores[r.nextInt(valores.length)];
+	}
+
+}
